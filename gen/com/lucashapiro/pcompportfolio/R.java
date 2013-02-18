db53b80c609d922bcd49b897a96a4bed9c581f5e
Luca <ls3101@nyu.edu>
2013-02-17 21:57:25 -0500

    message

--- gen/com/lucashapiro/pcompportfolio/R.java
+++ gen/com/lucashapiro/pcompportfolio/R.java
@@ -12,26 +12,42 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int image1=0x7f020001;
+        public static final int image2=0x7f020002;
+        public static final int image3=0x7f020003;
+        public static final int image4=0x7f020004;
+        public static final int image5=0x7f020005;
+        public static final int image6=0x7f020006;
     }
     public static final class id {
-        public static final int addcode=0x7f070005;
-        public static final int addcommentary=0x7f070004;
-        public static final int addphotos=0x7f070006;
-        public static final int addtitle=0x7f070008;
+        public static final int addcode=0x7f070007;
+        public static final int addcommentary=0x7f070006;
+        public static final int addphotos=0x7f070008;
+        public static final int addtitle=0x7f07000a;
         public static final int browseprojects=0x7f070003;
-        public static final int menu_settings=0x7f07000c;
+        public static final int button1=0x7f070011;
+        public static final int button2=0x7f070010;
+        public static final int imageView1=0x7f070005;
+        public static final int imageView2=0x7f07000f;
+        public static final int imageView3=0x7f07000d;
+        public static final int menu_settings=0x7f070015;
         public static final int newproject=0x7f070002;
-        public static final int oldproject1=0x7f070009;
-        public static final int oldproject2=0x7f07000a;
+        public static final int oldproject1=0x7f07000c;
+        public static final int oldproject2=0x7f07000e;
         public static final int oldproject3=0x7f07000b;
-        public static final int oldprojects=0x7f070001;
-        public static final int textView1=0x7f070000;
-        public static final int uploadtoblog=0x7f070007;
+        public static final int oldprojects=0x7f070004;
+        public static final int textView1=0x7f070001;
+        public static final int thatproject=0x7f070012;
+        public static final int theotherproject=0x7f070013;
+        public static final int thisproject=0x7f070014;
+        public static final int uploadtoblog=0x7f070009;
+        public static final int videoview=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_new_project=0x7f030001;
         public static final int activity_old_projects=0x7f030002;
+        public static final int browse_projects=0x7f030003;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
--- src/com/lucashapiro/pcompportfolio/MainActivity.java
+++ src/com/lucashapiro/pcompportfolio/MainActivity.java
@@ -1,16 +1,33 @@
 package com.lucashapiro.pcompportfolio;
 
+import java.io.IOException;
+import java.io.InputStream;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.view.LayoutInflater;
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.widget.VideoView;
 
 public class MainActivity extends Activity implements OnClickListener {
 	
 	Button newProject;
 	Button oldProjects;
 	Button browseProjects;
+	ImageView imageView;
+	LayoutInflater inflater;
+	View browseView;
+	
+	//from Browse Projects
+	Button thisProject;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +37,51 @@ public class MainActivity extends Activity implements OnClickListener {
 		newProject = (Button) this.findViewById(R.id.newproject);
 		newProject.setOnClickListener(this);
 		
+		oldProjects = (Button) this.findViewById(R.id.oldprojects);
+		oldProjects.setOnClickListener(this);
+		
+		browseProjects = (Button) this.findViewById(R.id.browseprojects);
+		browseProjects.setOnClickListener(this);
+		
+		imageView = (ImageView) this.findViewById(R.id.imageView1);
+		
+		inflater = LayoutInflater.from(this);
+        browseView = inflater.inflate(R.layout.browse_projects, null);
+		
+        thisProject = (Button) this.findViewById(R.id.thisproject);
+		thisProject.setOnClickListener(this);
 	}
 
 	@Override
 	public void onClick(View v) {
-		setContentView(R.layout.activity_new_project);
+		if (v == newProject) {
+			setContentView(R.layout.activity_new_project);
+		}
+		if (v == oldProjects) {
+			setContentView(R.layout.activity_old_projects);
+		}
+		if (v == browseProjects) {
+			setContentView(R.layout.browse_projects);
+		}
+		if (v == thisProject) {
+			setContentView(R.layout.activity_new_project);
+		}
 		
-		
+	}
+	
+	public class ViewTheVideo extends Activity 
+	{
+	    VideoView vv;
+
+	    @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.activity_main);
+	        vv = (VideoView) this.findViewById(R.id.videoview);
+			Uri videoUri = Uri.parse(Environment.getExternalStorageDirectory().getPath() + "/TestMovie.mp4");
+	        vv.setVideoURI(videoUri);
+	        vv.start();
+	    }
 	}
 
 }
